d52ace485bfef6712085642bee3390f69cb2e237
Oskar R Laptop <o.rajzner@gmail.com>
2023-05-28 12:41:20 +0200

    Add gradle and dependencies, init commit

---
+++ src/main/java/com/viepovsky/ConstructionToolsApp.java
@@ -0,0 +1,13 @@
+package com.viepovsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionToolsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionToolsApp.class, args);
+	}
+
+}
